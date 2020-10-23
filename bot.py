a5209302d57740e7d6ed7b1c469ea53a59ee26d7
Taufan Raifaldy Maaruf <taufan.maaruf@gmail.com>
2020-10-24 01:10:51 +0700

    Added validation for mentioning user

--- bot.py
+++ bot.py
@@ -12,4 +12,18 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    content = message.content
+    
+    if content.startswith('!friday vote'):
+        mentions = message.mentions
+
+        if len(mentions) == 0:
+            await message.channel.send("You didn't mention anyone yet!, Please mention someone")
+            return
+        elif len(mentions) > 1:
+            await message.channel.send("Please mention 1 person only!")
+            return
+
 client.run(TOKEN)
